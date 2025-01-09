b1fc7fab15a0561c212c097163126e034bedcb06
danchami <yinwq20@lzu.edu.cn>
2025-01-09 15:10:08 +0800

    Site updated: 2025-01-09 15:10:07

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1736406410535";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1736406578034";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
